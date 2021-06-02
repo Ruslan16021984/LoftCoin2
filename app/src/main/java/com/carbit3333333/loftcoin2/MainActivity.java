dbebd31a3aa7cc6d0ae92b6ab1b4717d1a12bbb9
Ruslan16021984 <carbit3333333@gmail.com>
2021-06-02 20:21:25 +0300

    commit1

---
+++ app/src/main/java/com/carbit3333333/loftcoin2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.carbit3333333.loftcoin2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
